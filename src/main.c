0d6d48a8edc3c2e6da41da8d84d100099692c712
Annamalai <an.nachiappan06@gmail.com>
2026-08-13 18:19:39 +0100

    Added uart debug output

--- src/main.c
+++ src/main.c
@@ -1,26 +1,46 @@
+//#define CLOCK_FREQ 12000000UL
 #include <inttypes.h>
 #include <stdbool.h>
+#define USARTDIV 48000000/115200
 const int GPIO_BANK_NUMBER = 5;
+const int CLOCK_FREQ = 48000000;
 
 
 //GPIO code
 
 struct GPIO{
-    volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
+    volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFRL, AFRH;
 };
 
 struct SYS_TICK{
-    volatile uint32_t SYST_CSR, SYST_RVR, SYST_CVR, SYST_CALIB;
-    
+    volatile uint32_t SYST_CSR, SYST_RVR, SYST_CVR, SYST_CALIB;  
 };
 
+struct UART{
+    volatile uint32_t CR1, CR2, CR3, BRR, GTPR, RTQR, RQR, ISR, ICR, RDR, TDR, PRESC;
+    /*
+    - BRR -> set the baud rate, for UART and USART this is the amount of data transmitted per second
+    - GTPR -> sets the guard time value(transmission complete flag set after this 
+    time is drained), and sets prescaler value which divides system clock -> both these values are only
+     accessed in certain modes and not in normal mode
+    - RTOR -> receiver timeout register, sets a flag after some time 
+    where nothing is to be read
+    - RQR -> used to make requests like discard data without reading it, or put USART in mute mode
+    - ISR -> gives information on the status of the USART like a busy flag if there is comms on the RX line, or RX stack is full
+    - ICR -> clears flags of ISR, same idea of BSRR used for ODR
+    - RDR -> contains data character received
+    - TDR -> contains data character to be transmitted
+    - PRESC -> used to divide the input clock by some number 
+    */
+};
 
 typedef struct GPIO GPIO;
 typedef struct SYS_TICK SYS_TICK;
+typedef struct UART UART;
 typedef enum {
     GPIO_MODE_INPUT,//you will be reading from these registers
     GPIO_MODE_OUTPUT, 
-    GPIO_MODE_AF,//you will call some alternate behaviour elsewhere
+    GPIO_MODE_AF,//this maps the pin as input for or output of(whether I or O depends on AF number and the pin) some other peripheral like USART, SPI, ...
     GPIO_MODE_ANALOG//you just use the actual analog value of the GPIO pin rather than interpreting it a binary value
     //you could sample the value and take it as input for ADC
 } GPIO_mode;
@@ -35,9 +55,33 @@ const int RCC_BASE = 0x40021000;
 //To save power in STM32 all peripherals are turned off but not the case in most other MCUs
 //This gives a clock source to components    
 uint32_t* RCC_IOPENR = (uint32_t*)(RCC_BASE + 0x34) ;
+uint32_t* RCC_APBENR1 = (uint32_t*)(RCC_BASE + 0x3C) ;
 SYS_TICK* const SYS_TICKp = (SYS_TICK*)0xE000E010;
 
+//Only uart1 and uart2 have RX and TX ports mapped to GPIO pins on stm32c031c6 
+UART* const uart1 = (UART*)0x40013800;
+UART* const uart2 = (UART*)0x40004400;
+UART* const uart3 = (UART*)0x40004800;
+UART* const uart4 = (UART*)0x40004C00;
 
+static inline int powInt(int base, int exp){
+    int ans = 1;
+    while(exp){
+        ans *= base;
+        exp--;
+    }
+    return ans;
+}
+
+/*
+I think this should not have been made into a function, code like 5*2 will now have to be executed
+and pow will be executed so it would have been more efficient to just call the normal code.
+*/
+static inline void setReg(volatile uint32_t* reg, int index, int stride, int val){
+    int oneMask = (int)((powInt(2, stride)) - 1);
+    *reg &= (uint32_t)(~(((oneMask)) << (index*stride)));//this 0s out what is in bits you will change so that following mask will work and it does not affect other bits
+    *reg |= (uint32_t)(((val)&(oneMask)) << (index*stride));
+}
 
 inline GPIO *getGPIO(char bank){
     //GPIO *gpioPins[GPIO_BANK_NUMBER] = {0, 0, 0, 0, 0};
@@ -49,9 +93,18 @@ inline GPIO *getGPIO(char bank){
 static inline void setModeGPIO(char bank, int pinNum, GPIO_mode gM){
     GPIO *gpioBank = getGPIO(bank);
     //pin numbers start from 0 so you can have A0 and mode for pin number stored as two bits in pinNum*2 and pinNum* + 1
-    gpioBank->MODER &= ~(3U << (pinNum*2));//this 0s out what is in bits you will change so that following mask will work and it does not affect other bits
-    gpioBank->MODER |= ((gM & 3U) << (pinNum*2));
+    setReg(&(gpioBank->MODER), pinNum, 2, gM);
     //U is added after 3 to make it unsigned to avoid any strange errors that may happen
+}
+
+static inline void setAltFuncGPIO(char bank, int pinNum, int afNum){
+    GPIO *gpioBank = getGPIO(bank);
+    if(pinNum <= 7){
+        setReg(&(gpioBank->AFRL), pinNum, 4, afNum);
+    }else if(pinNum >= 8 && pinNum <= 15){
+        setReg(&(gpioBank->AFRH), pinNum - 8, 4, afNum);//setReg requires an index so you need to - 8
+    }
+    
 }
 
 static inline void writeGPIO(char bank, int pinNum, int val){
@@ -66,7 +119,8 @@ static inline void writeGPIO(char bank, int pinNum, int val){
     on BSRR which will reset the bit on ODR
     - There are only 0 to 15 bits on ODR
     - You can modify the ODR directly but writing one bit require a read(taking ODR value 
-    into register), modify(bitwise OR), write(back to ODR) series of steps
+    into register as you want to preserve the values in other bits), modify(bitwise 
+    OR), write(back to ODR) series of steps
     - This violates atomicity so after you have read if there is an interrupt and 
     it modifies some other bit of same ODR and then your modify and write happens
     , this would mean that the modification made by interrupt would be lost as you
@@ -138,18 +192,59 @@ bool timerExpire(uint32_t* lastTick, uint32_t prd, uint32_t timeNow){
     return entered;
 }
 
+void delay(int N){
+    while(N--){
+        asm("nop");
+    }
+}
 
+void initSerialMonitor(){
+    //Setting up GPIO pins
+    setModeGPIO('A', 2, GPIO_MODE_AF);
+    setModeGPIO('A', 3, GPIO_MODE_AF);
+    setAltFuncGPIO('A', 2, 1);
+    setAltFuncGPIO('A', 3, 1);
+    //Enabling USART peripheral
+    *RCC_APBENR1 |= (1 << 17); //you are only changing one bit so no need to zero things out(would be necessary for storing eg 01)
+    (void)*RCC_APBENR1; // Dummy read forces CPU to wait for clock stabilization
+    uart2->CR1 = 0;//UART(from setting UE bit to 0) needs to be disabled for some bits to be set
+    uart2->BRR = (uint32_t)(48000000/115200);
+    uart2->CR1 = 13;
+}
+
+void writeToSerialMonitor(char* msg){
+    while(*msg != 0){
+        uart2->TDR = (uint8_t)(*msg);
+        msg++;
+        while((uart2->ISR & (1 << 7)) == 0){
+            /*
+            This is TXE bit which is used to show TDR is free and the data in there has 
+            been moved to shift register so you can write there
+            There is a TC bit which shows the whole transmission is complete so shift register is empty 
+            and TX line is IDLE. This is used right at the end so that you don't disable 
+            the USART when there is data in shift register for example. But TC does not seem to work
+             on STM32 when I use it here. For example, using TC should mean loop runs longer but no 
+             change in output while only first letter gets printed in reality.
+            */
+            delay(1);
+        };
+    }
+    
+}
 
 int main(void){
-    systickInit(48000);
+    systickInit(CLOCK_FREQ/1000);
     *RCC_IOPENR |= 1;
     setModeGPIO('A', 10, GPIO_MODE_OUTPUT);
+    initSerialMonitor();
+    writeToSerialMonitor("HELLO\n");
     
     static bool on = true;
     uint32_t prd = 2000;
     uint32_t lastTick = s_ticks;
     while(1){
         if(timerExpire(&lastTick, prd, s_ticks)){
+            writeToSerialMonitor("HELLO\n");
             writeGPIO('A', 10, on);
             on = !on;
         }
@@ -172,7 +267,7 @@ This is stuff that needs to be stored in non-volatile memory so that its durable
 but to be used by MCU when chip is booted it needs to be in RAM.
 */
 
-__attribute__((naked, noreturn)) void _reset(void){
+__attribute__((noreturn)) void _reset(void){
     /*
     naked 
     - means no function epilogue(restores stack for parent) and no function prologue(this is where stack is set up eg required number of bytes for variables allocated)    
